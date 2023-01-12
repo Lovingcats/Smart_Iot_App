32434ca24040b63a6239bd5c3a44058a2a34d21c
조용제 <whehdrms6810@naver.com>
2023-01-12 10:35:12 +0900

    [ADD] basic code

---
+++ lib/screens/randompage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:smart_iot/common/common.dart';
+
+class RandomPage extends StatefulWidget {
+  const RandomPage({super.key});
+
+  @override
+  State<RandomPage> createState() => _RandomPageState();
+}
+
+class _RandomPageState extends State<RandomPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CommonColor.blue,
+      appBar: null,
+      body: Center(
+        child: Column(
+
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
