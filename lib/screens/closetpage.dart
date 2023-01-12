9d0e5d5f1e9392b2e68c01880b5e4346e27b76c2
조용제 <whehdrms6810@naver.com>
2023-01-12 10:34:59 +0900

    [ADD first ui

---
+++ lib/screens/closetpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ClosetPage extends StatefulWidget {
+  const ClosetPage({super.key});
+
+  @override
+  State<ClosetPage> createState() => _ClosetPageState();
+}
+
+class _ClosetPageState extends State<ClosetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
